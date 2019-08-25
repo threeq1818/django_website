82f643655b213cfcfc9d5870d8b572137d4a849f
Three Q <threeq1818@gmail.com>
2019-08-26 02:37:35 +0800

    added  'accounts/' url create login template in root/templates/registration/ directory

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -24,3 +24,8 @@ urlpatterns = [
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]

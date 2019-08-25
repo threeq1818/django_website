23f92d99bf22c90354708d046b7502a81d74c2f6
Three Q <threeq1818@gmail.com>
2019-08-26 00:39:43 +0800

    added author_list, author_detail 	create templates and add url, insert some code into view.py modify the book_detail for connection with author info

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -60,3 +60,34 @@ class BookDetailView(generic.DetailView):
     def book_detail_view(request, primary_key):
         book = get_object_or_404(Book, pk=primary_key)
         return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+    # # your own name for the list as a template variable
+    # context_object_name = 'my_book_list'
+    # # Get 5 books containing the title war
+    # queryset = Book.objects.filter(title__icontains='war')[:5]
+    # # Specify your own template name/location
+    # template_name = 'books/my_arbitrary_template_name_list.html'
+
+    def get_queryset(self):
+        # Get 5 books containing the title war
+        return Author.objects.filter()[:5]
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def author_detail_view(request, primary_key):
+        author = get_object_or_404(Author, pk=primary_key)
+        return render(request, 'catalog/author_detail.html', context={'author': author})
